85d980a9cfe976a1acbf1fa5bf73bf56c1ed1584
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2020-11-30 13:22:37 +0100

    Creating admin module

---
+++ models/AdminsModel.php
@@ -0,0 +1,160 @@
+<?php
+    namespace Models;
+
+    use Core\Out;
+    use Core\Model;
+    use Ekolo\Builder\Http\Request;
+
+    class AdminsModel extends Model {
+
+        public function __construct()
+        {
+            parent::__construct();
+            $this->setTable('admins');
+        }
+
+        /**
+         * Création d'un admin
+         * @param Request $req
+         * @return Out
+         */
+        public function createAdmin(Request $req)
+        {
+            $data = [
+                'name' => $req->body()->name,
+                'firstName' => $req->body()->firstName,
+                'email' => $req->body()->email,
+                'phone' => $req->body()->phone,
+                'username' => $req->body()->username,
+                'password' => \bcrypt_hash_password($req->body()->password),
+            ];
+
+            if (!$this->exists('email', $data['email'])) {
+                if (!$this->exists('username', $data['username'])) {
+                    if (!empty($result = $this->add($data))) {
+                        $this->out->state = true;
+                        $this->out->message = "Admin créé avec succès";
+                        $this->out->result = $result;
+                    }else {
+                        $this->out->message = "Une erreur est survenue lors de la création admin";
+                    }
+                }else {
+                    $this->out->message = "Nom d'utilisateur déjà utilisé";
+                }
+            }else {
+                $this->out->message = "Cette adresse email est déjà utilisé";
+            }
+
+            return $this->out;
+        }
+
+        /**
+         * Connexion d'un utilisateur
+         * @param Request $req
+         * @return Out
+         */
+        public function loginAdmin(Request $req)
+        {
+            $out = new Out;
+            $Admin = $this->findOne([
+                'cond' => 'email="'.$req->body()->email.'"'
+            ]);
+            
+            if (!empty($Admin)) {
+                if (bcrypt_verify_password($req->body()->password, $Admin->password)) {
+                    $AdminAgent = $req->server()->get('HTTP_Admin_AGENT');
+                    $system = "";
+
+                    if (preg_match_all("#\(.{1,}\)#i", $AdminAgent, $groupMatchSystem)) {
+                        $system = $groupMatchSystem[0][0];
+                    }
+
+                    $logged = $this->add([
+                        'idAdmin' => $Admin->id,
+                        'kit' => $AdminAgent,
+                        'system' => $system
+                    ], 'session_Admin');
+
+                    if ($logged) {
+                        $out->state = true;
+                        $out->message = "Vous êtes connecté avec succès";
+                        $out->result = $Admin;
+                    }else {
+                        $out->message = "Une erreur est survenue lors de la connexion, réessayez";
+                    }
+                }else {
+                    $out->message = "Mot de passe saisi est incorrect !";
+                }
+            }else {
+                $out->message = "Email ou Mot de passe incorrect !";
+            }
+
+            return $out;
+        }
+
+        /**
+         * Déconnexion d'un utilisateur
+         * @param array $data
+         * @return Out
+         */
+        public function logout(array $data)
+        {
+            $out = new Out;
+            $Admin = $this->update([
+                'idAdmin' => $data['id_Admin'],
+                'dateLogout' => $data['dateLogout']
+            ], 'session_Admin', 'id_Admin');
+            
+            if ($Admin) {
+                $out->state = true;
+                $out->message = "Vous êtes déconnecté avec succès";
+                $out->result = $Admin;
+            }else {
+                $out->message = "Une erreur est survenue lors de la déconnexion !";
+            }
+
+            return $out;
+        }
+
+        /**
+         * Modification compte utilisateur
+         * @param Request $req
+         * @param int $id
+         */
+        public function updateAdmin(Request $req, $id)
+        {
+            $out = new Out;
+            $data = [
+                'id' => (int) $id,
+                'name' => $req->body()->name,
+                'firstName' => $req->body()->firstName,
+                'email' => $req->body()->email,
+                'phone' => $req->body()->phone,
+                'id_country' => $req->body()->country,
+                'town' => $req->body()->town,
+                'adress' => $req->body()->adress,
+                'avatar' => $req->body()->avatar,
+                'about' => $req->body()->about
+            ];
+
+            $emailUsed = $this->findOne([
+                'cond' => 'email = "'.$data['email'].'" AND id != '.$id.' AND flag="true"'
+            ]);
+            
+
+            if (empty($emailUsed)) {
+                if (!empty($result = $this->update($data))) {
+                    $out->state = true;
+                    $out->message = "Mise à jour réussie !";
+                    $out->result = $result;
+                }else {
+                    $out->message = "Une erreur est survenue lors de la mise à jour";
+                }
+            }else {
+                $out->message = "Cette adresse email est déjà utilisé";
+            }
+
+            return $out;
+        }
+
+    }
---
+++ routes/api/admins.php
@@ -0,0 +1,197 @@
+<?php
+    use Ekolo\Builder\Routing\Router;
+    use Ekolo\Builder\Http\Request;
+    use Ekolo\Builder\Http\Response;
+    use Core\Validator;
+    use Core\Out;
+    use Models\AdminsModel;
+
+    $router = new Router;
+    $modelAdmin = new AdminsModel;
+    $out = new Out;
+
+    /**
+     * Route création admin
+     */
+    $router->post('/create', function (Request $req, Response $res) {
+        global $modelAdmin;
+        global $out;
+        
+        $validator = new Validator($req);
+
+        $validator->setRules([
+            'name' => 'required|min:2',
+            'firstName' => 'required|min:2',
+            'email' => 'required|email',
+            'phone' => 'required|tel',
+            'password' => 'required|min:8',
+            'username' => 'required|min:2',
+        ]);
+
+        if ($validator->validator()) {
+            $out = $modelAdmin->createAdmin($req);
+        }else {
+            $out->message = implode("<br>", session('errors'));
+            $out->message = str_replace('name', 'Nom', $out->message);
+            $out->message = str_replace('firstName', 'Prénom', $out->message);
+            $out->message = str_replace('phone', 'Téléphone', $out->message);
+            $out->message = str_replace('username', 'Nom d\'utilisateur', $out->message);
+            $out->message = str_replace('password', 'Mot de passe', $out->message);
+
+            session('errors', null);
+            session()->remove('errors');
+        }
+
+        $res->json($out);
+    });
+
+    /**
+     * Inscription d'un utilisateur
+     */
+    $router->post('/register', function (Request $req, Response $res) {
+
+        $model = new UsersModel;
+        $out = new Out;
+        $validator = new Validator($req);
+
+        $validator->setRules([
+            'name' => 'required|min:2',
+            'email' => 'required|email',
+            'password' => 'required|min:8'
+        ]);
+
+        if ($validator->validator()) {
+            $user = [
+                'name' => $req->body()->name,
+                'email' => $req->body()->email,
+                'password' => $req->body()->password
+            ];
+
+            $out = $model->registerUser($user);
+        }else {
+            $out->message = implode("<br>", session('errors'));
+            $out->message = str_replace('name', 'Nom', $out->message);
+            $out->message = str_replace('password', 'Mot de passe', $out->message);
+        }
+
+        $res->json($out);
+    });
+
+     /**
+     * Connexion d'un utilisateur
+     */
+    $router->post('/login', function (Request $req, Response $res) {
+
+        $model = new UsersModel;
+        $out = new Out;
+        $validator = new Validator($req);
+
+        $validator->setRules([
+            'email' => 'required|email',
+            'password' => 'required|min:8'
+        ]);
+
+        if ($validator->validator()) {
+            $out = $model->loginUser($req);
+
+            if ($out->state) {
+                session('user', [
+                    'state' => $out->result->state,
+                    'name' => $out->result->name,
+                    'firstName' => $out->result->firstName,
+                    'lastName' => $out->result->lastName,
+                    'email' => $out->result->email,
+                    'id' => $out->result->id
+                ]);
+
+                // if ($req->body()->remember) {
+                //     setcookie('user_remember', base64_encode(encode_id($out->result->id).'_'.$out->result->email), 3600 * 30);
+                // }
+            }
+        }else {
+            $out->message = implode("<br>", session('errors'));
+            $out->message = str_replace('password', 'Mot de passe', $out->message);
+        }
+
+        $res->json($out);
+    });
+
+    /**
+     * Déconnexion d'un utilisateur
+     */
+    $router->put('/logout', function (Request $req, Response $res) {
+
+        $model = new UsersModel;
+
+        $out = $model->logout([
+            'id_user' => session('user')['id'],
+            'dateLogout' => date('Y-m-d h:i:s')
+        ]);
+
+        if ($out->state) {
+            session('user', null);
+            session()->remove('user');
+        }
+
+        $res->json($out);
+    });
+
+    /**
+     * Récupération des informations d'un utilisateur
+     */
+    $router->get('/infos/:id', function (Request $req, Response $res) {
+
+        $model = new UsersModel;
+
+        $out = $model->logout([
+            'id_user' => session('user')['id'],
+            'dateLogout' => date('Y-m-d h:i:s')
+        ]);
+
+        if ($out->state) {
+            session('user', null);
+            session()->remove('user');
+        }
+
+        $res->json($out);
+    });
+
+    /**
+     * Inscription d'un utilisateur
+     */
+    $router->put('/update', function (Request $req, Response $res) {
+
+        $model = new UsersModel;
+        $out = new Out;
+        $validator = new Validator($req);
+
+        $validator->setRules([
+            'name' => 'required|min:2',
+            'email' => 'required|email',
+            'firstName' => 'required|min:2',
+            'phone' => 'required|tel',
+            'country' => 'required|int',
+            'town' => 'required|alpha|min:1',
+            'adress' => 'required|alpha|min:3'
+        ]);
+
+        if ($validator->validator()) {
+            $out = $model->updateUser($req, session('user')['id']);
+        }else {
+            session('errors')['country'] = "Pays invalide";
+
+            $out->message = implode("<br>", session('errors'));
+            $out->message = str_replace('name', 'Nom', $out->message);
+            $out->message = str_replace('firstName', 'Prénom', $out->message);
+            $out->message = str_replace('phone', 'Téléphone', $out->message);
+            $out->message = str_replace('country', 'Pays', $out->message);
+            $out->message = str_replace('town', 'Ville', $out->message);
+            $out->message = str_replace('adress', 'Adresse', $out->message);
+
+            session()->remove('errors');
+        }
+
+        $res->json($out);
+    });
+    
+    return $router;
