15f17c5b40894843cc917b264feecb5250a5e637
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2020-12-03 12:36:44 +0100

    Ajout du formulaire d'enregistrement d'un nouveau type

--- views/dashboard/admin/types.php
+++ views/dashboard/admin/types.php
@@ -1,12 +1,35 @@
 <div class="row small-spacing">
-    <div class="col-xs-12">
-        <h3>Liste de types de catégories</h3>
+    <div class="col-sm-4 col-xs-12">
+        <div class="box-content card white">
+            <h4 class="box-title">Nouveau type</h4>
+            <!-- /.box-title -->
+            <div class="card-content">
+                <form>
+                    <div class="form-group">
+                        <label for="intituled">Intitulé <span class="text-danger">*</span></label>
+                        <input type="text" class="form-control" id="intituled" />
+                    </div>
+                    <div class="form-group">
+                        <label for="description">Description (facultatif)</label>
+                        <textarea name="description" id="description" class="form-control"></textarea>
+                    </div>
+                    
+                    <div class="text-center">
+                        <button type="submit" class="btn btn-primary btn-sm waves-effect waves-light">Enregistrer <i class="fa fa-save"></i></button>
+                    </div>
+                </form>
+            </div>
+            <!-- /.card-content -->
+        </div>
+        <!-- /.box-content -->
+    </div>
+    <div class="col-sm-8 col-xs-12">
         <div class="box-content">
+            <h3 class="box-title">Liste de types de catégories</h3>
             <table id="table-types-categories" class="display" style="width: 100%">
                 <thead>
                     <tr>
                         <th class="text-center">#</th>
-                        <th class="text-center">Date</th>
                         <th>Intitulé</th>
                         <th>Descriptions</th>
                         <th class="text-center">Actions</th>
@@ -15,7 +38,6 @@
                 <tfoot>
                     <tr>
                         <th class="text-center">#</th>
-                        <th class="text-center">Date</th>
                         <th>Intitulé</th>
                         <th>Descriptions</th>
                         <th class="text-center">Actions</th>
@@ -25,7 +47,6 @@
                     <?php for ($i = 1;$i < 6;$i++) : ?>
                     <tr>
                         <td class="text-center"><?= $i ?></td>
-                        <td class="text-center">12/10/2009</td>
                         <td>Livre</td>
                         <td>Alex</td>
                         <td class="text-center">
