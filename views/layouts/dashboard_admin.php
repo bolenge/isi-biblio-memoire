329891bb30bcec495e93ac7b343660bb53c7dd95
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2020-12-09 13:00:29 +0100

    Ajout du nom admin logged

--- views/layouts/dashboard_admin.php
+++ views/layouts/dashboard_admin.php
@@ -53,15 +53,15 @@
             <div class="user">
                 <a href="#" class="avatar"><img src="/public/img/default-avatar.png" alt=""><span
                         class="status online"></span></a>
-                <h5 class="name"><a href="profile.html">Emily Stanley</a></h5>
+                <h5 class="name"><a href="#"><?= session('admin')['firstName'].' '.session('admin')['name'] ?></a></h5>
                 <h5 class="position">Administrator</h5>
                 <!-- /.name -->
                 <div class="control-wrap js__drop_down">
                     <i class="fa fa-caret-down js__drop_down_button"></i>
                     <div class="control-list">
-                        <div class="control-item"><a href="profile.html"><i class="fa fa-user"></i> Profile</a></div>
-                        <div class="control-item"><a href="#"><i class="fa fa-gear"></i> Settings</a></div>
-                        <div class="control-item"><a href="#"><i class="fa fa-sign-out"></i> Log out</a></div>
+                        <div class="control-item"><a href="/admin/profile"><i class="fa fa-user"></i> Profile</a></div>
+                        <div class="control-item"><a href="/admin/reset-password"><i class="fa fa-gear"></i> Changer mot de passe</a></div>
+                        <div class="control-item"><a href="/admin/logout"><i class="fa fa-sign-out"></i> Déconnexion</a></div>
                     </div>
                     <!-- /.control-list -->
                 </div>
