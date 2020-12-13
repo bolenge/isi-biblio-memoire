e6af546a82ced3446768a095f310631e869782d2
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2020-12-13 07:36:59 +0100

    Ajout de ckeditor dans public/vendor

--- views/dashboard/admin/books/waiting.php
+++ views/dashboard/admin/books/waiting.php
@@ -32,18 +32,18 @@
                         <?php foreach ($books as $book) : ?>
                             <tr>
                                 <td><?= $i ?></td>
-                                <td><?= $book->title ?></td>
+                                <td title="<?= $book->title ?>"><?= sub_string($book->title, 25) ?></td>
                                 <td><?= $book->category ?></td>
                                 <td><?= $book->other ?></td>
                                 <td><?= format_date($book->createdAt) ?></td>
-                                <td>
-                                    <a href="<?= $book->fileDoc ?>" download="<?= $book->fileDoc ?>">Docs <i class="fa fa-file-word-o"></i></a> |
-                                    <a href="#">Audio <i class="fa fa-play"></i></a>
+                                <td class="text-center">
+                                    <a href="<?= $book->fileDoc ?>" download="<?= $book->fileDoc ?>"> <i class="fa fa-file-word-o"></i></a> |
+                                    <a href="#"><i class="fa fa-play"></i></a>
                                 </td>
                                 <td class="text-center">
                                     <a href="#" class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></a>
                                     <a href="#" class="btn btn-danger btn-xs"><i class="fa fa-trash-o"></i></a>
-                                    <a href="#" class="btn btn-success btn-xs">Valider et Publier</a>
+                                    <a href="#" class="btn btn-success btn-xs"><i class="fa fa-send"></i></a>
                                 </td>
                             </tr>
                             <?php $i++ ?>
