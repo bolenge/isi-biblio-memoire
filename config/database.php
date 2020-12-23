b363b4cfe5fbbab318d4257bb29d9304471989a2
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2020-12-23 12:44:23 +0100

    Some updating of config/database

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
     /**
      * Fichier de configuration de la base de données
-     * @author Don de Dieu BOLENGE <dondedieubolenge@gmail.com>
+     * @author Don de Dieu BOLENGE <dondedieubolenge@gmail.com>  
      */
     return [
         'DB_HOST' => 'localhost',
@@ -9,3 +9,13 @@
         'DB_USERNAME' => 'root',
         'DB_PASSWORD' => '',
     ];
+
+    /**
+     * On line config
+     */
+    // return [
+    //     'DB_HOST' => 'localhost',
+    //     'DB_DATABASE' => 'zamengap_congobook',
+    //     'DB_USERNAME' => 'zamengap_congobo',
+    //     'DB_PASSWORD' => 'QQY@zCi3T5vd64@',
+    // ];
