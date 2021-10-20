da8ff8fa8dd7e3be5ec0efbf5f21c9826cdf941d
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-10-20 09:24:15 +0100

    fix errors

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -25,7 +25,7 @@ class ComposerAutoloaderInit408f35912c8abacaa0fb1b316ba44e6b
         require __DIR__ . '/platform_check.php';
 
         spl_autoload_register(array('ComposerAutoloaderInit408f35912c8abacaa0fb1b316ba44e6b', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit408f35912c8abacaa0fb1b316ba44e6b', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
